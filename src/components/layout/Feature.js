bc546bebb45ebe1c71efcb104ea1fe3eebbd498f
yinonhever <yinonehever@gmail.com>
2020-08-16 13:22:03 +0300

    update

--- src/components/layout/Feature.js
+++ src/components/layout/Feature.js
@@ -1,15 +1,18 @@
 import React from "react";
+import Zoom from "react-reveal/Zoom";
 
 const Feature = props => (
-    <div className="feature">
-        <img className="feature__img" src={props.img} alt="illustration" />
-        <h3 className="feature__title">
-            {props.heading}
-        </h3>
-        <p className="paragraph feature__text">
-            {props.children}
-        </p>
-    </div>
+    <Zoom duration={600}>
+        <div className="feature">
+            <img className="feature__img" src={props.img} alt="illustration" />
+            <h3 className="feature__title">
+                {props.heading}
+            </h3>
+            <p className="paragraph feature__text">
+                {props.children}
+            </p>
+        </div>
+    </Zoom>
 )
 
 export default Feature;
