0d3bb9d0cae356cb6ba4a6dedcf13b17f71b2c43
yinonhever <yinonehever@gmail.com>
2020-08-17 16:50:32 +0300

    adding the footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,9 +4,29 @@ import Logo from "../UI/Logo";
 
 const Footer = () => (
     <footer className="footer">
-        <Link to="/" style={{ display: "block" }}>
-            <Logo white />
-        </Link>
+        <div className="footer__container">
+            <div className="footer__left">
+                <Link to="/">
+                    <Logo white />
+                </Link>
+                <div className="footer__navigation">
+                    <Link to="/" className="footer__link">About</Link>
+                    <Link to="/works" className="footer__link">Works</Link>
+                    <Link to="/contact" className="footer__link">Contact</Link>
+                </div>
+            </div>
+            <div className="footer__right">
+                <a
+                    className="footer__link"
+                    rel="noopener noreferrer"
+                    href="https://github.com/yinonhever"
+                    target="_blank">
+                    <i className="footer__link-icon fab fa-github" />
+                    <span>View on Github</span>
+                </a>
+                <p className="footer__copyright">&copy; 2020 Yinon Hever. All right reserved</p>
+            </div>
+        </div>
     </footer>
 )
 
