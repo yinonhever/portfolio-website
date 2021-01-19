59075fa3c2202a2320813ad2dae27ec5fab3b0d9
yinonhever <yinonehever@gmail.com>
2021-01-19 19:07:24 +0200

    update

--- src/components/layout/Intro.js
+++ src/components/layout/Intro.js
@@ -8,7 +8,7 @@ const Intro = () => (
             <div className="intro__content">
                 <h1 className="intro__heading">Powerful web development</h1>
                 <p className="paragraph intro__text">
-                    I'm Yinon, a front-end web developer. I'm dedicated to building and improving
+                    I'm Yinon, a full-stack web developer. I'm dedicated to building and improving
                     beautiful websites and web apps that stand out, with advanced designs, optimized
                     functionality and complete user experience.
                 </p>
