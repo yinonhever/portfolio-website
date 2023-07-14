f19b49553751715ec73d0064eaa8c052955ac23a
yinonhever <yinonehever@gmail.com>
2023-07-14 22:22:38 +0300

    fix UI bug in selecting gallery image

--- src/containers/Page.js
+++ src/containers/Page.js
@@ -1,20 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import PageHeading from "../components/layout/PageHeading";
 
 const Page = props => {
-    const { title, children } = props;
+  const { title, children } = props;
 
-    document.title = title ? `${title} – Yinon Hever` : "Yinon Hever – Portfolio Website";
+  useEffect(() => {
+    document.title = title
+      ? `${title} – Yinon Hever`
+      : "Yinon Hever – Portfolio Website";
     window.scrollTo(0, 0);
+  }, [title]);
 
-    return (
-        <Fragment>
-            {title && <PageHeading title={title} />}
-            <main className="page-container">
-                {children}
-            </main>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      {title && <PageHeading title={title} />}
+      <main className="page-container">{children}</main>
+    </Fragment>
+  );
+};
 
 export default Page;
