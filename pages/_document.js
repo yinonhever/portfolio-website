a86a934f6a22ced0d9ab1ad6ce54e550dcd68dfd
yinonhever <yinonehever@gmail.com>
2025-08-19 15:46:26 +1200

    attempt to fix missing favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&family=Spartan:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
